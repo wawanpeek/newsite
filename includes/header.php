3b6a6647bbbb2a8eff342dedcb8a359b09efeaf4
shamebin <angelinesambun@gmail.com>
2021-06-09 16:30:18 +0800

    created project and folder structure

---
+++ includes/header.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/site.css" />
+    <title>Attendance - <?php echo $title?> </title>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">TMFlix</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav mr-auto">
+    
+    <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
+          <a class="nav-item nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+        
+          <a class="nav-item nav-link" href="series.php">TV Shows</a>
+        
+          <a class="nav-item nav-link" href="mylist.php">My List</a>
+       </div>
+       <div class="navbar-nav ml-auto">
+       <a class="nav-item nav-link" href="#">Login</a>
+      </ul>
+    </div>
+  </div>
+ </nav>
+ <br/>
+
+ <div class="container">
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+    $title = 'Index';
+    require_once 'includes/header.php'; 
+?>
+
+    <h1>Trending</h1>
+
+<?php require_once 'includes/footer.php'; ?>
