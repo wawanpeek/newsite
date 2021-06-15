39a05fca0a6395640e8e57db9dba59e3f22e54c4
shamebin <84175691+shamebin@users.noreply.github.com>
2021-06-15 15:29:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php 
+    require_once 'includes/header.php'; 
+?>
+    <h3>Trending</h3>
+
+    <div class="container">
+  <div class="row align-items-start">
+    <div class="col">
+      One of three columns<a href="goodgirls/season.php">
+     <input type="submit"/>
+  
+   
+      <img src="goodgirls.jpg" alt="good girls poster" class="img-fluid" />  </a>
+      
+      
+      ?>
+    </div>
+    <div class="col">
+      One of three columns
+      <img src="goodgirls.jpg" alt="good girls poster" class="img-fluid" />
+    </div>
+    <div class="col">
+      One of three columns
+      <img src="goodgirls.jpg" alt="good girls poster" class="img-fluid" />
+    </div>
+  </div>
+  <div class="row align-items-center">
+    <div class="col">
+      One of three columns
+      <img src="goodgirls.jpg" alt="good girls poster" class="img-fluid" />
+    </div>
+    <div class="col">
+      One of three columns
+      <img src="guardian.jpg" alt="good girls poster" class="img-fluid" />
+    </div>
+    <div class="col">
+      One of three columns
+      <img src="ratched.jpeg" alt="good girls poster" class="img-fluid" />
+    </div>
+  </div>
+  <div class="row align-items-end">
+    <div class="col">
+      One of three columns
+      <img src="goodgirls.jpg" alt="good girls poster" class="img-fluid" />
+    </div>
+    <div class="col">
+      One of three columns
+      <img src="goodgirls.jpg" alt="good girls poster" class="img-fluid" />
+    </div>
+    <div class="col">
+      One of three columns
+      <img src="goodgirls.jpg" alt="good girls poster" class="img-fluid" />
+    </div>
+  </div>
+</div>
+
+<?php require_once 'includes/footer.php'; ?>
