df4db6f3affd3f95200189b70d58facb9eb78db9
shamebin <84175691+shamebin@users.noreply.github.com>
2021-06-15 15:31:43 +0800

    Add files via upload

---
+++ goodgirls/season.php
@@ -0,0 +1,6 @@
+<?php 
+    require_once 'includes/header.php'; 
+?>
+<h3>Testing bijes</h3>
+
+<?php require_once 'includes/footer.php'; ?>
---
+++ includes/header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="css/site.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" >
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans&display=swap" rel="stylesheet">
+    <title> <?php echo $title?> </title>
+    
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+    
+    <p class="navbar-nav mx-auto"><a class="navbar-brand" href="#" ><img src="tmflix.png" alt="TMFLIX"> </a></p>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav mx-auto" class="container-fluid" >
+    
+        
+          <a class="nav-item nav-link" href="index.php">Home</a>
+        
+          <a class="nav-item nav-link" href="series.php">TV Shows</a>
+        
+          <a class="nav-item nav-link" href="mylist.php">My List</a>
+
+          <a class="nav-item nav-link" href="index.php">Account</a>
+       </div>
+
+       <div class="navbar-nav ml-auto" class="container-fluid">
+    
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button type="button" class="btn btn-outline-danger" >Search</i></i></button>
+    </div>
+  </div>
+ </nav>
+ <br/>
+
+ <div class="container">
